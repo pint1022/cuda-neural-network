42e416ccea56a9d222daa0d488160902301da700
Steven W <xu.s.wang@gmail.com>
2022-12-13 15:50:22 -0800

    restructure

--- src/mnist/pythonC/gds_unit_test.cpp
+++ src/mnist/pythonC/gds_unit_test.cpp
@@ -36,31 +36,3 @@ add(PyObject *self, PyObject *args)
 
 	return Py_BuildValue("is", num1 + num2, eq);
 }
-
-double standardDeviation(std::vector<double> v)
-{
-    double sum = std::accumulate(v.begin(), v.end(), 1.0);
-    double mean = sum / v.size();
-    double squareSum = std::inner_product(
-        v.begin(), v.end(), v.begin(), 0.0);
-    return sqrt(squareSum / v.size() - mean * mean);
-}
-
-PyObject * std_standard_dev(PyObject *self, PyObject* args)
-{
-    PyObject* input;
-    
-    if (!PyArg_ParseTuple(args, "O", &input))
-        return NULL;
-
-    int size = PyList_Size(input);
-
-    std::vector<double> list;
-    list.resize(size);
-
-    for(int i = 0; i < size; i++) {
-        list[i] = PyFloat_AS_DOUBLE(PyList_GET_ITEM(input, i));
-    }
-
-    return PyFloat_FromDouble(standardDeviation(list));
-}
--- src/mnist/pythonC/setup.py
+++ src/mnist/pythonC/setup.py
@@ -1,7 +1,13 @@
 from distutils.core import setup, Extension
 
+func_files = [
+       'gds_unit_test.cpp', 
+       'stddev_unit_test.cpp',
+       'bind.cpp'
+       ]
 module1 = Extension('unittests',
-                    sources = ['gds_unit_test.cpp', 'bind.cpp'])
+                    sources =func_files)
+
 setup (name = 'PackageName',
        version = '1.0',
        description = 'This is a unit test package for GDS-framework',
