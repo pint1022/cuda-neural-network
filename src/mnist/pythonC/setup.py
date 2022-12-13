884bcb55f5bbbce103981d703a0313f29181d9c5
Steven W <xu.s.wang@gmail.com>
2022-12-13 15:37:45 -0800

    change to c++

--- src/mnist/pythonC/setup.py
+++ src/mnist/pythonC/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('unittests',
-                    sources = ['gds_unit_test.c', 'bind.c'])
+                    sources = ['gds_unit_test.cpp', 'bind.cpp'])
 setup (name = 'PackageName',
        version = '1.0',
        description = 'This is a unit test package for GDS-framework',
--- src/mnist/pythonC/test.py
+++ src/mnist/pythonC/test.py
@@ -1,6 +1,46 @@
 #!/usr/bin/env python3
-
+import numpy as np
+import random
+import timeit
+import pandas as pd
+from matplotlib import pyplot as plt
 import unittests as gds
 
-gds.system("ls -l")
-print(gds.add(5, 6));
+def data_gen(rg):
+    lp_time = []
+    py_time = []
+    np_time = []
+    c_time = []
+
+    for l in rg:
+        rands = [random.random() for _ in range(0, l)]
+        numpy_rands = np.array(rands)
+        np_time = np.append(np_time, timeit.timeit(lambda: np.std(numpy_rands), number=1000))
+        # print(l, np_time.shape)
+        c_time = np.append(c_time, timeit.timeit(lambda: gds.standard_dev(rands), number=1000))
+    return np.array([np.transpose(np_time), np.transpose(c_time)])
+
+def test_stddev():
+    lens = range(1000, 20000, 1000)
+    data = data_gen(rg=lens)
+
+    df = pd.DataFrame(data.transpose(), index=lens, columns=['Numpy', 'C++'])
+    plt.figure()
+    df.plot()
+    plt.legend(loc='best')
+    plt.ylabel('Time (Seconds)')
+    plt.xlabel('Number of Elements')
+    plt.title('1k Runs of Standard Deviation')
+    plt.savefig('numpy_vs_c.png')
+    plt.show()
+
+def test_system():
+    gds.system("ls -l")
+
+def test_add():
+    print(gds.add(5, 6))
+
+test_stddev()
+test_system()
+test_add()
+
