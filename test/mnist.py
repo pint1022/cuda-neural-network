183ea238c46e915b937b59f4ad7d917bd2f594ce
Steven W <xu.s.wang@gmail.com>
2023-01-10 11:33:39 -0800

    better mnist.py

--- test/mnist.py
+++ test/mnist.py
@@ -11,7 +11,14 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.optimizers import SGD
- 
+
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import datetime
+import time
+import os
+
 # load train and test dataset
 def load_dataset():
     # load dataset
@@ -118,6 +125,68 @@ def run_test_harness():
     summarize_diagnostics(histories)
     # summarize estimated performance
     summarize_performance(scores)
- 
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+def simple_test(epochs=5):
+
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+
+    ds_train = ds_train.map(
+        normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    ds_train = ds_train.batch(128)
+    ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+    ds_test = ds_test.map(
+        normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test = ds_test.batch(128)
+    ds_test = ds_test.cache()
+    ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
+        tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+    log_dir = 'rlogs/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, profile_batch = '500,520')
+    #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    start = time.perf_counter()
+    # print('before training, memory usage: {}'.format(tf.config.experimental.get_memory_usage('GPU:0')))
+
+    with tf.device("/gpu:0"):
+        model.fit(
+                ds_train,
+                epochs=epochs,
+                validation_data=ds_test,
+                callbacks=[tensorboard_callback]
+            )
+
+    elapsed = time.perf_counter() - start
+
+    # print('after training, memory usage: {}'.format(tf.config.experimental.get_memory_usage('GPU:0')))
+    print('Elapsed %.3f seconds.' % elapsed)
+
 # entry point, run the test harness
-run_test_harness()
+# run_test_harness()
+simple_test()
