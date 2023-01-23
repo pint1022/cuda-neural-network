8cae19e3ad5c290654cffd1c1d98776fecba074b
Steven W <xu.s.wang@gmail.com>
2023-01-23 15:51:22 -0800

    restructure

--- test/cuda/util_func.cu
+++ test/cuda/util_func.cu
@@ -4,7 +4,102 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 #include <chrono>
-#include "util_func.cuh"
+
+template<typename T>
+void initialize_matrix(T* M, int rows, int cols, std::function<float()> F) {
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < cols; j++){
+      M[i * cols + j] = F();
+    }
+  }
+}
+
+template<typename T>
+void initialize_matrix(T* M, int rows, int cols, std::function<float(int, int)> F) {
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < cols; j++){
+      M[i * cols + j] = F(i, j);
+    }
+  }
+}
+
+template<typename T>
+void print_matrix(T* M, int rows, int cols) {
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < cols; j++){
+        std::cout << M[i * cols + j] << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+
+template<typename T>
+T maxDiff(T* A1, T* A2, int rows, int cols){
+  T maxDiff = A1[0] - A2[0];
+  for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < cols; j++){
+      T diff = abs(A1[i * cols + j] - A2[i * cols + j]);
+      if( diff > maxDiff) {
+          maxDiff = diff;
+      }
+    }
+  }
+
+  
+  return maxDiff;
+}
+
+template<typename T>
+void check_copy(T* dM, T* hM, int d_size, char* label) {
+  T* cp_host = (T*) malloc(d_size*sizeof(T));
+
+  std::cout << "\n\n" << label << std::endl;
+
+	cudaMemcpy(cp_host, dM, d_size, cudaMemcpyDeviceToHost);
+  std::cout << "CPU original" << std::endl;
+  print_matrix<T>( hM, 10, 10);
+  std::cout << "GPU copy" << std::endl;
+  print_matrix<T>( cp_host, 10, 10);
+
+  free(cp_host);
+}
+
+#define ROW_TILE_WIDTH  32
+#define COL_TILE_WIDTH  32
+
+#define EPSILON         (1e-5)
+
+template<typename T>
+__global__
+void naive_matrix_multiply(T *A, T *B, T* C, int width, int cRows, int cCols)
+{
+  __shared__ T shATile[ROW_TILE_WIDTH][COL_TILE_WIDTH];
+  __shared__ T shBTile[ROW_TILE_WIDTH][COL_TILE_WIDTH];
+
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+  T pValue = 0;
+
+  // iterate for width/COL_TILE_WIDTH number of times
+  // to compute the C tile
+  for(int p = 0; p < width/COL_TILE_WIDTH; p++) {
+    //load values to tiles from A and B
+    shATile[threadIdx.y][threadIdx.x] = A[row * width + p * ROW_TILE_WIDTH + threadIdx.x];
+    shBTile[threadIdx.y][threadIdx.x] = B[(p * COL_TILE_WIDTH + threadIdx.y) * cCols + col];
+
+    // wait until all threads finish loading values
+    __syncthreads();
+    // update pValue for this thread
+    for(int i = 0; i < COL_TILE_WIDTH;  i++) pValue += shATile[threadIdx.y][i] * shBTile[i][threadIdx.x];
+    // wait until all threads finish computing pValue before overwriting
+    __syncthreads();
+
+  }
+  
+  C[row * cCols + col] = pValue;
+
+}
 
 template<typename T>
 void naive_matrix_multiply_cpu(T *A, T *B, T* C, int width, int C_rows, int C_cols){
@@ -21,9 +116,7 @@ void naive_matrix_multiply_cpu(T *A, T *B, T* C, int width, int C_rows, int C_co
     }
 }
 
-
-int main(void)
-{
+extern "C" void perform_matmul() {
   int A_rows = 1 << 8;
   int A_cols = 1 << 10;
   int B_cols = 1 << 11;
@@ -125,4 +218,3 @@ int main(void)
   free(B_cpu);
   free(C_cpu);  
 }
-
